c1ea9d9704bb97fcad0fe1d732b567de878b683c
Scott Windsor <swindsor@gmail.com>
2020-12-01 22:08:44 -0800

    2020 day 2 part 2

--- 2020/ruby/day02.rb
+++ 2020/ruby/day02.rb
@@ -4,14 +4,27 @@
 class TheLastPass
   def initialize input
     @entries = input.split("\n").map do |line|
-      line.match(/(?<min>\d+)-(?<max>\d+) (?<char>[a-z]): (?<password>[a-z]+)/)
+      line.match(/(?<from>\d+)-(?<to>\d+) (?<char>[a-z]): (?<password>[a-z]+)/)
     end
   end
 
-  def valid_count
+  def min_max_count
     @entries.select do |entry|
       count = entry[:password].count(entry[:char])
-      count <= entry[:max].to_i && count >= entry[:min].to_i
+      max = entry[:to].to_i 
+      min = entry[:from].to_i 
+      count <= max && count >= min
+    end.size
+  end
+
+  def index_count
+    @entries.select do |entry|
+      contain = entry[:from].to_i 
+      not_contain = entry[:to].to_i 
+
+      [entry[:password][contain-1],
+       entry[:password][not_contain-1]]
+        .count(entry[:char]) == 1
     end.size
   end
 end
@@ -19,5 +32,5 @@ end
 if __FILE__ == $0
   input = ARGF.read.chomp
   pass = TheLastPass.new input
-  puts pass.valid_count
+  puts pass.index_count
 end
--- 2020/ruby/day02_test.rb
+++ 2020/ruby/day02_test.rb
@@ -6,7 +6,7 @@ require "minitest/pride"
 require_relative "day02.rb"
 
 class TheLastPassTest < Minitest::Test
-  def test_password_matches
+  def test_password_matches_times
     input = <<~EOS
     1-3 a: abcde
     1-3 b: cdefg
@@ -15,6 +15,18 @@ class TheLastPassTest < Minitest::Test
 
     pass = TheLastPass.new input
 
-    assert_equal 2, pass.valid_count
+    assert_equal 2, pass.min_max_count
+  end
+
+  def test_password_matches_index
+    input = <<~EOS
+    1-3 a: abcde
+    1-3 b: cdefg
+    2-9 c: ccccccccc
+    EOS
+
+    pass = TheLastPass.new input
+
+    assert_equal 1, pass.index_count
   end
 end
