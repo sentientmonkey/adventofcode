0eea7441263c4eadd998b9f7963b2c63a8714e46
Scott Windsor <swindsor@gmail.com>
2020-12-03 22:49:18 -0800

    2020 day 4 part 2

--- 2020/ruby/day04.rb
+++ 2020/ruby/day04.rb
@@ -9,8 +9,18 @@ class Passport
   # ecl (Eye Color)
   # pid (Passport ID)
   # cid (Country ID)
-  def initialize
+  FIELDS = %w(byr iyr eyr hgt hcl ecl pid cid)
+
+  def initialize record=""
     @values = {}
+    parse record
+  end
+
+  def parse record
+    record.split(/\s+/).each_with_object(self) do |col,hash|
+      key, value = col.split ':'
+      hash[key] = value
+    end
   end
 
   def [](key)
@@ -21,20 +31,37 @@ class Passport
     @values[key] = value
   end
 
+  def field_valid? field
+    value = @values[field].to_s
+    case field
+    when 'byr'
+      value.to_i.between?(1920,2002)
+    when 'iyr'
+      value.to_i.between?(2010,2020)
+    when 'eyr'
+      value.to_i.between?(2020,2030)
+    when 'hgt'
+      (value.match?(/^\d+cm$/) && value.to_i.between?(150,193)) ||
+        (value.match?(/^\d+in$/) && value.to_i.between?(59,76))
+    when 'hcl'
+      value.match?(/^#[0-9a-f]{6}/)
+    when 'ecl'
+      %w(amb blu brn gry grn hzl oth).include? value
+    when 'pid'
+      value.match?(/^\d{9}$/)
+    when 'cid'
+      true
+    end
+  end
+
   def valid?
-    @values.length == 8 ||
-      (@values.length == 7 && !@values['cid'])
+    FIELDS.all?{ |field| field_valid? field }
   end
 end
 
 class PassportChecker
   def initialize input
-    @records = input.split(/\n{2}/).map do |record|
-      record.split(/\s+/).each_with_object(Passport.new) do |col,hash|
-        key, value = col.split ':'
-        hash[key] = value
-      end
-    end
+    @records = input.split(/\n{2}/).map { |r| Passport.new r }
   end
 
   def [](index)
--- 2020/ruby/day04_test.rb
+++ 2020/ruby/day04_test.rb
@@ -47,4 +47,99 @@ class PassportCheckerTest < Minitest::Test
 
     assert_equal 2, @checker.valid_count
   end
+
+  def field_valid? field, value
+    passport = Passport.new("#{field}:#{value}")
+    passport.field_valid?(field)
+  end
+
+  def test_bry
+    refute field_valid? "byr", 1919
+    assert field_valid? "byr", 1920
+    assert field_valid? "byr", 2002
+    refute field_valid? "byr", 2003
+  end
+
+  def test_iry
+    refute field_valid? "iyr", 2009
+    assert field_valid? "iyr", 2010
+    assert field_valid? "iyr", 2020
+    refute field_valid? "iyr", 2021
+  end
+
+  def test_hgt
+    refute field_valid? "hgt", "149cm"
+    assert field_valid? "hgt", "150cm"
+    assert field_valid? "hgt", "193cm"
+    refute field_valid? "hgt", "194cm"
+
+    refute field_valid? "hgt", "58in"
+    assert field_valid? "hgt", "59in"
+    assert field_valid? "hgt", "76in"
+    refute field_valid? "hgt", "77in"
+  end
+
+  def test_hcl
+    assert field_valid? "hcl", "#123abc"
+    refute field_valid? "hcl", "#123abz"
+    refute field_valid? "hcl", "123abc"
+  end
+
+  def test_ecl
+    assert field_valid? "ecl", "brn"
+    refute field_valid? "ecl", "wat"
+  end
+
+  def test_pid
+    assert field_valid? "pid", "000000001"
+    refute field_valid? "pid", "0123456789"
+  end
+
+  def test_cid
+    assert field_valid? "cid", "whatev"
+    assert Passport.new.field_valid? "cid"
+  end
+
+  def passport_valid? record
+    Passport.new(record).valid?
+  end
+
+  def test_valid
+    checker = PassportChecker.new <<~EOS
+    pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+    hcl:#623a2f
+
+    eyr:2029 ecl:blu cid:129 byr:1989
+    iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+    hcl:#888785
+    hgt:164cm byr:2001 iyr:2015 cid:88
+    pid:545766238 ecl:hzl
+    eyr:2022
+
+    iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+    EOS
+
+    assert_equal 4, checker.valid_count
+  end
+
+  def test_invalid
+    checker = PassportChecker.new <<~EOS
+    eyr:1972 cid:100
+    hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+    iyr:2019
+    hcl:#602927 eyr:1967 hgt:170cm
+    ecl:grn pid:012533040 byr:1946
+
+    hcl:dab227 iyr:2012
+    ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+    hgt:59cm ecl:zzz
+    eyr:2038 hcl:74454a iyr:2023
+    pid:3556412378 byr:2007
+    EOS
+
+    assert_equal 0, checker.valid_count
+  end
 end
