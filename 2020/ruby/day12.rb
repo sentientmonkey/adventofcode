79e9abf827821a287e07133d84dc37c7bba7fb5a
Scott Windsor <swindsor@gmail.com>
2020-12-12 00:37:26 -0800

    2020 day 12 part 2

--- 2020/ruby/day12.rb
+++ 2020/ruby/day12.rb
@@ -1,19 +1,57 @@
 #!/usr/bin/env ruby -w
 
+NORTH = 0 
+EAST = 90
+SOUTH = 180
+WEST = 270
+
 Pos = Struct.new(:x, :y) do
   def +(other)
     Pos.new x+other.x, y+other.y
+  end
+
+  def -(other)
+    Pos.new x-other.x, y-other.y
+  end
+
+  def >>(degrees)
+    case degrees
+    when 0
+      self
+    when 360
+      self
+    when 90
+      Pos.new(y,-x)
+    when 180
+      Pos.new(-x,-y)
+    when 270
+      Pos.new(-y,x)
+    else
+      raise "unknown direction #{degrees}"
+    end
+  end
+
+  def <<(degrees)
+    case degrees
+    when 0
+      self
+    when 360
+      self
+    when 90
+      self >> 270
+    when 180
+      self >> 180
+    when 270
+      self >> 90
+    else
+      raise "unknown direction #{degrees}"
+    end
   end
 
   def distance
     x.abs + y.abs
   end
 end
-
-NORTH = 0 
-EAST = 90
-SOUTH = 180
-WEST = 270
 
 Direction = Struct.new(:degrees) do
   def dxy(value)
@@ -46,25 +84,65 @@ South = Direction.new(SOUTH)
 West = Direction.new(WEST)
 
 class Ship
-  attr_reader :position, :direction
+  attr_reader :position, :direction, :waypoint
 
-  def initialize
+  def initialize waypoint: nil
     @position = Pos.new(0,0)
     @direction = Direction.new(90)
+    @waypoint = waypoint
   end
 
-  def sequence input
+  def sequence input, trace = false
     input.split("\n").each do |cmd|
       nav cmd
+      if trace
+        puts cmd
+        puts self
+      end
     end
+  end
+
+  def to_s
+    "pos:#{position.inspect} #{direction.inspect} way:#{waypoint.inspect}"
   end
 
   def distance
     @position.distance
   end
 
-  def nav command 
+  def nav command
     action, value = command[0], command[1...].to_i
+    if waypoint
+      waypoint_nav action, value
+    else
+      normal_nav action, value
+    end
+  end
+
+  def waypoint_nav action, value
+    case action
+    when 'F'
+      value.times do
+        @position += @waypoint
+      end
+    when 'N'
+      @waypoint += North.dxy(value)
+    when 'S'
+      @waypoint += South.dxy(value)
+    when 'E'
+      @waypoint += East.dxy(value)
+    when 'W'
+      @waypoint += West.dxy(value)
+    when 'R'
+      @waypoint >>= value
+    when 'L'
+      @waypoint <<= value
+    else
+      raise "unknown action: #{action}"
+    end
+  end
+
+  def normal_nav action, value 
     case action
     when 'F'
       @position += @direction.dxy(value)
@@ -84,6 +162,7 @@ class Ship
       raise "unknown action: #{action}"
     end
   end
+
 end
 
 if __FILE__ == $0
@@ -91,4 +170,8 @@ if __FILE__ == $0
   ship = Ship.new
   ship.sequence input
   puts ship.distance
+
+  waypoint_ship = Ship.new waypoint: Pos.new(10,1)
+  waypoint_ship.sequence input
+  puts waypoint_ship.distance
 end
--- 2020/ruby/day12_test.rb
+++ 2020/ruby/day12_test.rb
@@ -6,6 +6,7 @@ require_relative "day12.rb"
 class Test < Minitest::Test
   def setup
     @ship = Ship.new
+    @waypoint_ship = Ship.new waypoint: Pos.new(10,1)
   end
 
   def test_ship_moves_forward
@@ -43,4 +44,41 @@ class Test < Minitest::Test
     assert_equal Pos.new(17,-8), @ship.position
     assert_equal 25, @ship.distance
   end
+
+  def test_waypoint_move_forward
+    @waypoint_ship.nav "F10"
+    assert_equal Pos.new(100, 10), @waypoint_ship.position
+  end
+
+  def test_waypoint_move_north
+    @waypoint_ship.nav "N3"
+    assert_equal Pos.new(10, 4), @waypoint_ship.waypoint
+  end
+
+  def test_waypoint_rotate
+    @waypoint_ship.nav "N3"
+    @waypoint_ship.nav "R90"
+    assert_equal Pos.new(4,-10), @waypoint_ship.waypoint
+  end
+
+  def test_waypoint_rotate
+    @waypoint_ship.nav "N3"
+    @waypoint_ship.nav "L90"
+    assert_equal Pos.new(-4,10), @waypoint_ship.waypoint
+  end
+
+  def test_waypoint_nav_squence
+    input = <<~EOS
+    F10
+    N3
+    F7
+    R90
+    F11
+    EOS
+
+    @waypoint_ship.sequence input
+    assert_equal Pos.new(214,-72), @waypoint_ship.position
+    assert_equal 286, @waypoint_ship.distance
+  end
+
 end
