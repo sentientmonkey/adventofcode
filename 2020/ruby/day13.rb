4270069e6c26f1f13caafb1192ca87a801a0221c
Scott Windsor <swindsor@gmail.com>
2020-12-13 01:07:50 -0800

    2020 day 13 part 2 doesn't finish

--- 2020/ruby/day13.rb
+++ 2020/ruby/day13.rb
@@ -2,14 +2,16 @@
 
 
 class Timetable
-  attr_reader :start_time, :bus_routes
+  attr_reader :start_time, :bus_routes, :all_routes, :start_at
 
-  def initialize input
+  def initialize input, start_at:0
     first, rest = input.split("\n")
     @start_time = first.to_i
-    @bus_routes = rest.split(',')
+    @all_routes = rest.split(',')
+      .map{ |route| route == "x" ? route : route.to_i }
+    @bus_routes = @all_routes 
       .reject{ |route| route == "x" }
-      .map(&:to_i)
+    @start_at = start_at
   end
 
   def next_bus
@@ -21,10 +23,25 @@ class Timetable
   def stops? time
     bus_routes.find{ |route| time%route == 0 }
   end
+
+  def golden_timestamp
+    (start_at..).lazy
+      .each_cons(all_routes.size)
+      .find do |times| 
+          times.zip(all_routes).all? do |time, route| 
+            can_stop? time, route 
+          end
+      end.first
+  end
+
+  def can_stop? time, route
+    (route == "x" || time%route == 0)
+  end
 end
 
 if __FILE__ == $0
   input = ARGF.read.chomp
-  timetable = Timetable.new(input)
+  timetable = Timetable.new(input, start_at: 100000000000000)
   puts timetable.next_bus.reduce(&:*)
+  puts timetable.golden_timestamp
 end
--- 2020/ruby/day13_test.rb
+++ 2020/ruby/day13_test.rb
@@ -17,4 +17,29 @@ class TestTimetable < Minitest::Test
     assert_equal [7,13,59,31,19], @timetable.bus_routes
     assert_equal [5,59], @timetable.next_bus
   end
+
+  def test_find_golden_time
+    assert_equal [7,13,'x','x',59,'x',31,19], @timetable.all_routes
+    assert_equal 1068781, @timetable.golden_timestamp
+  end
+
+  def test_fime_golden_times
+    timetable = Timetable.new "0\n17,x,13,19"
+    assert_equal 3417, timetable.golden_timestamp
+
+    timetable = Timetable.new "0\n67,7,59,61"
+    assert_equal 754018, timetable.golden_timestamp
+
+    timetable = Timetable.new "0\n67,x,7,59,61"
+    assert_equal 779210, timetable.golden_timestamp
+
+    timetable = Timetable.new "0\n67,7,x,59,61"
+    assert_equal 1261476, timetable.golden_timestamp
+
+  end
+
+  def test_too_slow
+    timetable = Timetable.new "0\n1789,37,47,1889", start_at:1200000000
+    assert_equal 1202161486, timetable.golden_timestamp
+  end
 end
