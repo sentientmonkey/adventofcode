ade69810e6f83557861a4db401253fe9204670c7
Scott Windsor <swindsor@gmail.com>
2022-12-11 23:26:41 -0800

    2022 day 10 part 2 wip

--- 2022/racket/day10.rkt
+++ 2022/racket/day10.rkt
@@ -22,26 +22,54 @@
 (define (ins-arg ins)
   (cadr ins))
 
-(struct cpu (x pc sample) #:mutable #:transparent)
+(struct cpu (x pc sample crt) #:mutable #:transparent)
 
 (define (cpu-init)
-  (cpu 1 1 0))
+  (cpu 1 1 0 (make-vector 240 " ")))
 
 (define (sample-cpu c)
   (when (equal? (remainder (- (cpu-pc c) 20) 40) 0)
     (let ([s (* (cpu-x c) (cpu-pc c))])
       (set-cpu-sample! c (+ (cpu-sample c) s)))))
 
+(define (print-crt crt)
+  (for ([p (in-range 0 (vector-length crt))])
+    (display (vector-ref crt p))
+    (when (equal? (remainder (add1 p) 40) 0)
+      (display "\n"))))
+
+(define (sprite-over? x y)
+  (<= (sub1 x) y (add1 x)))
+
+(define (write-crt c)
+  (vector-set!
+    (cpu-crt c)
+    (sub1 (cpu-pc c))
+    (if (sprite-over? (cpu-x c) (remainder (cpu-pc c) 40))
+      "#"
+      ".")))
+
+(define (debug-crt c)
+  (printf "~a: ~a\n" (cpu-pc c) (cpu-x c))
+  (print-crt (cpu-crt c))
+  (displayln ""))
+
 (define (cpu-run c program)
   (for ([ins program])
+    (printf "~a: ~a\n" (cpu-pc c) (cpu-x c))
+    (print-crt (cpu-crt c))
+    (displayln "")
     (cond
       [(equal? (ins-op ins) 'noop)
        (sample-cpu c)
+       (write-crt c)
        (set-cpu-pc! c (add1 (cpu-pc c)))]
       [(equal? (ins-op ins) 'addx)
        (sample-cpu c)
+       (write-crt c)
        (set-cpu-pc! c (add1 (cpu-pc c)))
        (sample-cpu c)
+       (write-crt c)
        (set-cpu-pc! c (add1 (cpu-pc c)))
        (set-cpu-x! c (+ (ins-arg ins) (cpu-x c)))])))
 
@@ -58,8 +86,7 @@
   (test-case "CPU runs"
    (let ([c (cpu-init)])
      (cpu-run c '((noop) (addx 3) (addx -5)))
-     [check-equal? (cpu-x c) -1]
-     )))
+     [check-equal? (cpu-x c) -1])))
 
 (define (read-exercise-data)
   (~> (current-command-line-arguments)
@@ -71,4 +98,5 @@
   (let ([exercise-data (read-exercise-data)]
         [c (cpu-init)])
     (cpu-run c (parse exercise-data))
-    (println (cpu-sample c))))
+    (println (cpu-sample c))
+    (print-crt (cpu-crt c))))
