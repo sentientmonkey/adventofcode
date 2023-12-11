53031096a8a8d11de87e7b4338607e3cbac01937
Scott Windsor <swindsor@gmail.com>
2023-12-10 23:15:40 -0800

    2023 day 8 part two brute force

--- 2023/ruby/day08.rb
+++ 2023/ruby/day08.rb
@@ -1,17 +1,37 @@
 class Day08
-  attr_reader :directions, :network, :final_node
-  attr_accessor :step, :current_node
+  attr_reader :directions, :network, :final_nodes
+  attr_accessor :step, :current_nodes
 
-  def initialize(input)
+  def initialize(input, ghost_mode: false)
     @step = 0
-    @current_node = 'AAA'
-    @final_node = 'ZZZ'
+    @current_nodes = []
+    @final_nodes = []
     d, n = input.split("\n\n")
     @directions = d.chars
+    start_key = 'AAA'
+    end_key = 'ZZZ'
+    if ghost_mode
+      start_key = 'A'
+      end_key = 'Z'
+    end
     @network = n.split("\n").each_with_object({}) do |line, acc|
       line =~ /(\w+) = \((\w+), (\w+)\)/
-      acc[::Regexp.last_match(1)] = [::Regexp.last_match(2), ::Regexp.last_match(3)]
+      node = ::Regexp.last_match(1)
+      acc[node] = [::Regexp.last_match(2), ::Regexp.last_match(3)]
+      if node.end_with? start_key
+        @current_nodes << node
+      elsif node.end_with? end_key
+        @final_nodes << node
+      end
     end
+  end
+
+  def current_node
+    current_nodes.first
+  end
+
+  def final_node
+    final_nodes.first
   end
 
   def next_direction
@@ -20,28 +40,45 @@ class Day08
     directions[(step - 1) % directions.size]
   end
 
+  def at_end?
+    current_nodes.all? do |c|
+      c.end_with? 'Z'
+    end
+  end
+
   def find_steps
-    while current_node != final_node
+    until at_end?
+      # pp step
+      # pp current_nodes
       case next_direction
       when 'L'
-        self.current_node = left
+        self.current_nodes = left
       when 'R'
-        self.current_node = right
+        self.current_nodes = right
       end
     end
     self.step
   end
 
+  def fetch_current
+    current_nodes.map do |curr|
+      network.fetch(curr)
+    end
+  end
+
   def left
-    network.fetch(current_node)[0]
+    fetch_current.map(&:first)
   end
 
   def right
-    network.fetch(current_node)[1]
+    fetch_current.map(&:last)
   end
 end
 
 if __FILE__ == $0
-  exercise = Day08.new ARGF.read
+  input = ARGF.read
+  exercise = Day08.new input
+  puts exercise.find_steps
+  exercise = Day08.new input, ghost_mode: true
   puts exercise.find_steps
 end
--- 2023/ruby/day08_spec.rb
+++ 2023/ruby/day08_spec.rb
@@ -55,4 +55,47 @@ RSpec.describe Day08 do
       expect(exercise.find_steps).to eq(6)
     end
   end
+
+  context 'with ghost input' do
+    let(:exercise) do
+      described_class.new input, ghost_mode: true
+    end
+
+    let(:input) do
+      <<~INPUT
+        LR
+
+        11A = (11B, XXX)
+        11B = (XXX, 11Z)
+        11Z = (11B, XXX)
+        22A = (22B, XXX)
+        22B = (22C, 22C)
+        22C = (22Z, 22Z)
+        22Z = (22B, 22B)
+        XXX = (XXX, XXX)
+      INPUT
+    end
+
+    it 'should return current nodes' do
+      expect(exercise.current_nodes).to eq(
+        %w[
+          11A
+          22A
+        ]
+      )
+    end
+
+    it 'should return final nodes' do
+      expect(exercise.final_nodes).to eq(
+        %w[
+          11Z
+          22Z
+        ]
+      )
+    end
+
+    it 'should find steps' do
+      expect(exercise.find_steps).to eq(6)
+    end
+  end
 end
