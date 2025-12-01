da91ffea12f85708900bdf39d5d65bebc1232606
Scott Windsor <swindsor@gmail.com>
2025-12-01 00:40:12 -0800

    day 1 part 2 stuck

--- 2025/rust/src/bin/day01.rs
+++ 2025/rust/src/bin/day01.rs
@@ -16,26 +16,57 @@ fn read_file_or_stdin(path: &str) -> io::Result<String> {
     Ok(contents)
 }
 
-fn move_dial(command: &str, start: u32) -> u32 {
+fn move_dial(command: &str, start: i32) -> (i32, i32) {
     let (direction, amount_str) = command.split_at(1);
-    let mut amount: u32 = amount_str.parse().unwrap();
-    amount = amount % 100;
+    let raw_amount: i32 = amount_str.parse().unwrap();
+    let amount = raw_amount % 100;
+    let wraps = raw_amount / 100;
 
-    match direction {
-        "L" => (start + 100 - amount) % 100,
-        "R" => (start + amount) % 100,
+    let movement = match direction {
+        "L" => start - amount,
+        "R" => start + amount,
         _ => start,
-    }
+    };
+    let zeros = match direction {
+        "L" => {
+            if movement < 0 {
+                1
+            } else {
+                0
+            }
+        }
+        "R" => {
+            if movement >= 100 {
+                1
+            } else {
+                0
+            }
+        }
+        _ => 0,
+    };
+    let position = (movement + 100) % 100;
+
+    (position, zeros + wraps)
 }
 
-fn get_password(contents: &str) -> u32 {
+fn get_password(contents: &str) -> i32 {
     let mut position = 50;
     let mut password = 0;
     contents.lines().for_each(|line| {
-        position = move_dial(line, position);
+        (position, _) = move_dial(line, position);
         if position == 0 {
             password += 1;
         }
+    });
+    password
+}
+pub fn get_new_password(contents: &str) -> i32 {
+    let mut position = 50;
+    let mut password = 0;
+    let mut zeros = 0;
+    contents.lines().for_each(|line| {
+        (position, zeros) = move_dial(line, position);
+        password += zeros;
     });
     password
 }
@@ -43,9 +74,23 @@ fn get_password(contents: &str) -> u32 {
 #[cfg(test)]
 mod tests {
     use super::*;
+    const SAMPLE_INPUT: &str = "L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82";
 
-    fn test_move_dial(command: &str, start: u32, expected: u32) {
-        let result = move_dial(command, start);
+    fn test_move_dial(command: &str, start: i32, expected: i32) {
+        let (result, _) = move_dial(command, start);
+        assert_eq!(result, expected);
+    }
+    fn test_moves_past_zero(command: &str, start: i32, expected: i32) {
+        let (_, result) = move_dial(command, start);
         assert_eq!(result, expected);
     }
 
@@ -66,18 +111,21 @@ mod tests {
 
     #[test]
     fn it_returns_password() {
-        let contents = "L68
-L30
-R48
-L5
-R60
-L55
-L1
-L99
-R14
-L82";
-        let password = get_password(contents);
+        let password = get_password(SAMPLE_INPUT);
         assert_eq!(password, 3);
+    }
+
+    #[test]
+    fn it_moves_past_zero_left() {
+        test_moves_past_zero("L68", 50, 1);
+        test_moves_past_zero("L30", 82, 0);
+        test_moves_past_zero("R48", 52, 1);
+    }
+
+    #[test]
+    fn it_returns_new_password() {
+        let password = get_new_password(SAMPLE_INPUT);
+        assert_eq!(password, 6);
     }
 }
 
@@ -86,5 +134,7 @@ fn main() -> io::Result<()> {
     let contents = read_file_or_stdin(&input)?;
     let password = get_password(&contents);
     println!("{}", password);
+    let new_password = get_new_password(&contents);
+    println!("{}", new_password);
     Ok(())
 }
