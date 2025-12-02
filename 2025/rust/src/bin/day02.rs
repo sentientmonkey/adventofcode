69b23f36687a1f1571719f7c39c42a2405c92646
Scott Windsor <swindsor@gmail.com>
2025-12-02 13:19:21 -0800

    refactor

--- 2025/rust/src/bin/day02.rs
+++ 2025/rust/src/bin/day02.rs
@@ -16,7 +16,7 @@ fn read_file_or_stdin(path: &str) -> io::Result<String> {
     Ok(contents)
 }
 
-fn is_invalid_id(id: u64) -> bool {
+fn repeats(id: u64) -> bool {
     let s = id.to_string();
     let (first, second) = s.split_at(s.len() / 2);
     if first.len() != second.len() {
@@ -26,16 +26,16 @@ fn is_invalid_id(id: u64) -> bool {
     first.chars().zip(second.chars()).all(|(a, b)| a == b)
 }
 
-fn get_invalid_ids(start: u64, end: u64) -> Vec<u64> {
-    (start..end + 1).filter(|id| is_invalid_id(*id)).collect()
+fn get_repeating(start: u64, end: u64) -> Vec<u64> {
+    (start..end + 1).filter(|id| repeats(*id)).collect()
 }
 
-fn sum_of_invalid_ids(ids: &str) -> u64 {
+fn sum_of_repeating(ids: &str) -> u64 {
     ids.trim()
         .split(",")
         .map(|r| {
             let parts: Vec<u64> = r.splitn(2, "-").map(|s| s.parse().unwrap()).collect();
-            get_invalid_ids(parts[0], parts[1])
+            get_repeating(parts[0], parts[1])
         })
         .flatten()
         .sum()
@@ -44,7 +44,7 @@ fn sum_of_invalid_ids(ids: &str) -> u64 {
 fn main() -> io::Result<()> {
     let input = env::args().nth(1).unwrap_or_else(|| "-".to_string());
     let contents = read_file_or_stdin(&input)?;
-    let sum = sum_of_invalid_ids(&contents);
+    let sum = sum_of_repeating(&contents);
     println!("{}", sum);
     Ok(())
 }
@@ -55,24 +55,24 @@ mod tests {
     const SAMPLE_INPUT: &str = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
 
     #[test]
-    fn it_is_invalid_id() {
-        assert!(is_invalid_id(11));
-        assert!(!is_invalid_id(12));
-        assert!(!is_invalid_id(21));
-        assert!(is_invalid_id(22));
+    fn it_repeats() {
+        assert!(repeats(11));
+        assert!(!repeats(12));
+        assert!(!repeats(21));
+        assert!(repeats(22));
     }
 
     #[test]
-    fn it_returns_invalid_ids() {
-        assert_eq!(get_invalid_ids(11, 22), vec![11, 22]);
-        assert_eq!(get_invalid_ids(99, 115), vec![99]);
-        assert_eq!(get_invalid_ids(998, 1012), vec![1010]);
-        assert_eq!(get_invalid_ids(1188511880, 1188511890), vec![1188511885]);
-        assert_eq!(get_invalid_ids(222220, 222224), vec![222222]);
+    fn it_returns_repeating_ids() {
+        assert_eq!(get_repeating(11, 22), vec![11, 22]);
+        assert_eq!(get_repeating(99, 115), vec![99]);
+        assert_eq!(get_repeating(998, 1012), vec![1010]);
+        assert_eq!(get_repeating(1188511880, 1188511890), vec![1188511885]);
+        assert_eq!(get_repeating(222220, 222224), vec![222222]);
     }
 
     #[test]
-    fn it_returns_sum_of_invalid_ids() {
-        assert_eq!(sum_of_invalid_ids(SAMPLE_INPUT), 1227775554);
+    fn it_returns_sum_of_repeating() {
+        assert_eq!(sum_of_repeating(SAMPLE_INPUT), 1227775554);
     }
 }
