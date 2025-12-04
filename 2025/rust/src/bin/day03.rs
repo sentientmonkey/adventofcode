5f6eae185745385d1f90668f44cd8674aaa1ee10
Scott Windsor <swindsor@gmail.com>
2025-12-03 23:39:24 -0800

    day 3 part 2 stuck

--- 2025/rust/src/bin/day03.rs
+++ 2025/rust/src/bin/day03.rs
@@ -24,6 +24,39 @@ fn max_joltage(bank: &str) -> u64 {
         .unwrap()
 }
 
+fn max_joltagen(bank: &str, size: usize) -> u64 {
+    bank.chars()
+        .map(|s| s.to_digit(10).unwrap())
+        .fold(Vec::new(), |acc, x| {
+            let mut a = acc.clone();
+            if a.len() < size {
+                a.push(x);
+            } else {
+                for (i, n) in a.clone().iter().enumerate() {
+                    println!("i = {}, n = {}, x = {}", i, n, x);
+                    println!("a = {:?}", a);
+                    if i < a.len() - 1 && *n < a[i + 1] {
+                        a.remove(i);
+                        a.insert(i, *n);
+                        break;
+                    }
+                    if i == a.len() - 1 && x > *n {
+                        a.pop();
+                        a.push(x);
+                        break;
+                    }
+                }
+            }
+            a
+        })
+        .into_iter()
+        .map(|c| c.to_string())
+        .collect::<Vec<String>>()
+        .join("")
+        .parse()
+        .unwrap()
+}
+
 fn max_output(s: &str) -> u64 {
     s.lines().map(|l| max_joltage(l.trim())).sum()
 }
@@ -55,5 +88,12 @@ mod tests {
     #[test]
     fn it_finds_max_output() {
         assert_eq!(max_output(SAMPLE_INPUT), 357);
+    }
+
+    #[test]
+    fn it_finds_max_joltagn() {
+        assert_eq!(max_joltagen("987654321111111", 12), 987654321111);
+        assert_eq!(max_joltagen("811111111111119", 12), 811111111119);
+        assert_eq!(max_joltagen("234234234234278", 12), 434234234278);
     }
 }
