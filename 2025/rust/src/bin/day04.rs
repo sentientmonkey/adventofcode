44e5eb8cdf80bf14be69af2892ed0d27ce4b3299
Scott Windsor <swindsor@gmail.com>
2025-12-14 21:35:17 -0800

    cleanup and refactor

--- 2025/rust/src/bin/day04.rs
+++ 2025/rust/src/bin/day04.rs
@@ -1,11 +1,13 @@
 use adventofcode::read_file_or_stdin;
-use std::{env, io};
+use std::{env, fmt, io};
 
-fn content_to_grid(content: &str) -> Vec<Vec<char>> {
-    content
-        .lines()
-        .map(|l| l.chars().collect::<Vec<char>>())
-        .collect()
+fn content_to_grid(content: &str) -> Grid {
+    Grid::new(
+        content
+            .lines()
+            .map(|l| l.chars().collect::<Vec<char>>())
+            .collect(),
+    )
 }
 
 const NEIGHBORS: [[i32; 2]; 8] = [
@@ -19,88 +21,118 @@ const NEIGHBORS: [[i32; 2]; 8] = [
     [1, 1],
 ];
 
-fn print_grid(grid: &Vec<Vec<char>>) {
-    for row in grid.iter() {
-        for cell in row.iter() {
-            print!("{}", cell);
-        }
-        print!("\n");
-    }
-    print!("\n");
+#[derive(Clone)]
+struct Grid {
+    pub elements: Vec<Vec<char>>,
 }
 
-fn get_count(grid: &Vec<Vec<char>>, x: i32, y: i32) -> i32 {
-    let mut count = 0;
-    for d in NEIGHBORS {
-        let nx = x + d[0];
-        let ny = y + d[1];
-        if (0..grid.len() as i32).contains(&ny)
-            && (0..grid[ny as usize].len() as i32).contains(&nx)
-            && grid[ny as usize][nx as usize] == '@'
-        {
-            count += 1;
-        }
+impl Grid {
+    fn new(elements: Vec<Vec<char>>) -> Grid {
+        Grid { elements: elements }
     }
-    count
-}
-fn get_adjecent_count(grid: Vec<Vec<char>>) -> i32 {
-    let mut adj_count = 0;
-    for (y, row) in grid.iter().enumerate() {
-        for (x, _) in row.iter().enumerate() {
-            if grid[y][x] == '@' {
-                let count = get_count(&grid, x as i32, y as i32);
-                if count < 4 {
-                    adj_count += 1;
-                }
-            }
-        }
-    }
-    adj_count
-}
 
-fn remove_rolls(grid: Vec<Vec<char>>) -> (i32, Vec<Vec<char>>) {
-    let mut remove_count = 0;
-    let mut new_grid = grid.clone();
-    for (y, row) in grid.iter().enumerate() {
-        for (x, _) in row.iter().enumerate() {
-            match new_grid[y][x] {
-                '@' => {
-                    let count = get_count(&new_grid, x as i32, y as i32);
-                    if count < 4 {
-                        new_grid[y][x] = 'x';
-                        remove_count += 1;
-                    }
-                }
-                'x' => {
-                    new_grid[y][x] = '.';
+    fn get(&self, x: i32, y: i32) -> Option<char> {
+        if self.contains(x, y) {
+            Some(self.elements[y as usize][x as usize])
+        } else {
+            None
+        }
+    }
+
+    fn set(&mut self, x: i32, y: i32, c: char) {
+        self.elements[y as usize][x as usize] = c
+    }
+
+    fn contains(&self, x: i32, y: i32) -> bool {
+        (0..self.elements.len() as i32).contains(&y)
+            && (0..self.elements[y as usize].len() as i32).contains(&x)
+    }
+
+    fn get_count(&self, x: i32, y: i32) -> i32 {
+        let mut count = 0;
+        for d in NEIGHBORS {
+            let nx = x + d[0];
+            let ny = y + d[1];
+            match self.get(nx, ny) {
+                Some('@') => {
+                    count += 1;
                 }
                 _ => {}
             }
         }
+        count
     }
-    (remove_count, new_grid)
+
+    fn get_adjecent_count(&self) -> i32 {
+        let mut adj_count = 0;
+        for (y, row) in self.elements.iter().enumerate() {
+            for (x, _) in row.iter().enumerate() {
+                if self.get(x as i32, y as i32).unwrap() == '@' {
+                    let count = self.get_count(x as i32, y as i32);
+                    if count < 4 {
+                        adj_count += 1;
+                    }
+                }
+            }
+        }
+        adj_count
+    }
+    fn remove_rolls(&self) -> (i32, Self) {
+        let mut remove_count = 0;
+        let mut new_grid = self.clone();
+        for (y, row) in self.elements.iter().enumerate() {
+            for (x, _) in row.iter().enumerate() {
+                match new_grid.get(x as i32, y as i32) {
+                    Some('@') => {
+                        let count = new_grid.get_count(x as i32, y as i32);
+                        if count < 4 {
+                            new_grid.set(x as i32, y as i32, 'x');
+                            remove_count += 1;
+                        }
+                    }
+                    Some('x') => {
+                        new_grid.set(x as i32, y as i32, '.');
+                    }
+                    _ => {}
+                }
+            }
+        }
+        (remove_count, new_grid)
+    }
+
+    fn remove_all_rolls(&self) -> i32 {
+        let mut removed: i32 = 0;
+        let (mut count, mut new_grid) = self.remove_rolls();
+        removed += count;
+        while count > 0 {
+            (count, new_grid) = new_grid.clone().remove_rolls();
+            //println!("count = {}", count);
+            //println!("{}", new_grid);
+            removed += count;
+        }
+        removed
+    }
 }
 
-fn remove_all_rolls(grid: Vec<Vec<char>>) -> i32 {
-    let mut removed: i32 = 0;
-    let (mut count, mut new_grid) = remove_rolls(grid);
-    removed += count;
-    while count > 0 {
-        (count, new_grid) = remove_rolls(new_grid.clone());
-        //println!("count = {}", count);
-        //print_grid(&new_grid);
-        removed += count;
+impl fmt::Display for Grid {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for row in self.elements.iter() {
+            for cell in row.iter() {
+                write!(f, "{}", cell)?;
+            }
+            write!(f, "\n")?;
+        }
+        Ok(())
     }
-    removed
 }
 
 fn main() -> io::Result<()> {
     let input = env::args().nth(1).unwrap_or_else(|| "-".to_string());
     let contents = read_file_or_stdin(&input)?;
     let grid = content_to_grid(&contents);
-    let adj = get_adjecent_count(grid.clone());
+    let adj = grid.get_adjecent_count();
     println!("{:?}", adj);
-    let removed = remove_all_rolls(grid);
+    let removed = grid.remove_all_rolls();
     println!("{:?}", removed);
     Ok(())
 }
@@ -122,7 +154,7 @@ mod tests {
     #[test]
     fn it_should_remove_all_rolls() {
         let grid = content_to_grid(SAMPLE_INPUT);
-        let count = remove_all_rolls(grid);
+        let count = grid.remove_all_rolls();
         assert_eq!(count, 43);
     }
 }
